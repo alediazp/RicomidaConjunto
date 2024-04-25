31007c929836dff2a5d59747d15cc04d92689121
alediazp <alexisdiazperez@gmail.com>
2024-04-25 00:01:43 -0400

    agrego eventos a botones "enviar por correo" y "añadir a favoritos"

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
     $("#enviarCorreo").click(function () {
         alert("El correo fue enviado correctamente...");
     });
-}); 
+});
+
+$(function () {
+    $("#enviarCorreo").tooltip();
+    $("#agregaFavoritos").tooltip();
+});
